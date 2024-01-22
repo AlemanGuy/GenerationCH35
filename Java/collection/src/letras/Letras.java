96ed74b46272a57305f3d0f50c2f35d6a8634a08
AlemanGuy <alemancreativo@gmail.com>
2024-01-22 14:35:36 -0600

    +

---
+++ Java/collection/src/letras/Letras.java
@@ -0,0 +1,16 @@
+package letras;
+
+import java.util.Scanner;
+
+        public class Letras {
+		Scanner scanner = new Scanner (System.in);
+		
+	        public String leerEntrada() {
+			return scanner.nextLine();
+		}
+	        public void mostrarMensaje(String mensaje) {
+	        	System.out.println(mensaje);
+	        }
+	}
+
+
